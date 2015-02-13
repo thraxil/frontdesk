7a39c29161d7c8ced5779b47bb13307c9a5caec6
Anders Pearson <anders@columbia.edu>
2015-02-13 11:12:36 +0100

    split channellogger to its own file

---
+++ channellogger.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+
+	"github.com/boltdb/bolt"
+	irc "github.com/fluffle/goirc/client"
+)
+
+type channelLogger struct {
+	db      *bolt.DB
+	channel string
+}
+
+func newChannelLogger(db *bolt.DB, channel string) *channelLogger {
+	return &channelLogger{db, channel}
+}
+
+func (cl *channelLogger) Handle(conn *irc.Conn, line *irc.Line) {
+	fmt.Println(line.Nick, line.Text())
+	fmt.Println(line.Time)
+	if line.Target() == cl.channel {
+		fmt.Println("to the whole channel")
+		cl.logLine(line)
+	} else {
+		fmt.Println("to just me")
+		// process it for commands
+	}
+}
+
+func (cl *channelLogger) logLine(line *irc.Line) {
+	year, month, day := line.Time.Date()
+	le := lineEntry{normalizeNick(line.Nick), line.Text(), line.Time}
+	data, err := json.Marshal(le)
+	if err != nil {
+		log.Println("error marshalling to json")
+		log.Println(err)
+		return
+	}
+
+	err = cl.db.Update(func(tx *bolt.Tx) error {
+		lines := tx.Bucket([]byte("lines"))
+		ybucket, err := lines.CreateBucketIfNotExists([]byte(fmt.Sprintf("%04d", year)))
+		if err != nil {
+			return err
+		}
+		mbucket, err := ybucket.CreateBucketIfNotExists([]byte(fmt.Sprintf("%02d", month)))
+		if err != nil {
+			return err
+		}
+		dbucket, err := mbucket.CreateBucketIfNotExists([]byte(fmt.Sprintf("%02d", day)))
+		if err != nil {
+			return err
+		}
+		err = dbucket.Put([]byte(le.Key()), data)
+		return err
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("logged it...")
+}
--- frontdesk.go
+++ frontdesk.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"math"
@@ -108,27 +107,6 @@ func main() {
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", cfg.Port), nil))
 }
 
-type channelLogger struct {
-	db      *bolt.DB
-	channel string
-}
-
-func newChannelLogger(db *bolt.DB, channel string) *channelLogger {
-	return &channelLogger{db, channel}
-}
-
-func (cl *channelLogger) Handle(conn *irc.Conn, line *irc.Line) {
-	fmt.Println(line.Nick, line.Text())
-	fmt.Println(line.Time)
-	if line.Target() == cl.channel {
-		fmt.Println("to the whole channel")
-		cl.logLine(line)
-	} else {
-		fmt.Println("to just me")
-		// process it for commands
-	}
-}
-
 type lineEntry struct {
 	Nick      string
 	Text      string
@@ -141,39 +119,6 @@ func (l lineEntry) Key() string {
 
 func (l lineEntry) NiceTime() string {
 	return l.Timestamp.Format("15:04:05")
-}
-
-func (cl *channelLogger) logLine(line *irc.Line) {
-	year, month, day := line.Time.Date()
-	le := lineEntry{normalizeNick(line.Nick), line.Text(), line.Time}
-	data, err := json.Marshal(le)
-	if err != nil {
-		log.Println("error marshalling to json")
-		log.Println(err)
-		return
-	}
-
-	err = cl.db.Update(func(tx *bolt.Tx) error {
-		lines := tx.Bucket([]byte("lines"))
-		ybucket, err := lines.CreateBucketIfNotExists([]byte(fmt.Sprintf("%04d", year)))
-		if err != nil {
-			return err
-		}
-		mbucket, err := ybucket.CreateBucketIfNotExists([]byte(fmt.Sprintf("%02d", month)))
-		if err != nil {
-			return err
-		}
-		dbucket, err := mbucket.CreateBucketIfNotExists([]byte(fmt.Sprintf("%02d", day)))
-		if err != nil {
-			return err
-		}
-		err = dbucket.Put([]byte(le.Key()), data)
-		return err
-	})
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Println("logged it...")
 }
 
 // IRC likes to rename 'foo' to 'foo_', etc.
